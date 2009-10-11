b997b24556df4e488c9d2545462929a7a059d144
showa-yojyo <yojyo@hotmail.com>
2009-10-11 14:17:57 +0000

    * build.sh: HTML ファイルの出力先を "build" ディレクトリに変更
    git-svn-id: svn://localhost/note-svn/trunk@2 c94b6e96-04ce-b74c-b9b8-e4e85ce117fa

--- build.sh
+++ build.sh
@@ -2,11 +2,22 @@
 # $Id$
 
 RST2HTML=./rst2html.py
+CSSFILE=./note.css
+DEST=./build
 
+# build 先のディレクトリを確保
+mkdir -p ${DEST}
+
+# rst2html コンバート
 for SOURCE in *.txt ; do
   TARGET="${SOURCE%%.txt}.html"
-  if [ "$SOURCE" -nt "$TARGET" ] ; then
+  if [ "$SOURCE" -nt "$DEST/$TARGET" ] ; then
     echo Processing $SOURCE...
-    "$RST2HTML" "$SOURCE" "$TARGET"
+    "$RST2HTML" "$SOURCE" "$DEST/$TARGET"
   fi
 done
+
+# css ファイルを build 先ディレクトリへコピー
+if [ -f "$CSSFILE" ]; then
+  cp -u note.css ${DEST}
+fi
