3417adfe3e54a3a3f0badc9819474f8ce765278c
showa-yojyo <yojyo@hotmail.com>
2010-10-03 11:10:08 +0000

    * conf.py (copyright): 2010 に修正  * prefab-theme/static/default.css_t (h1): margin-top を指定して隙間を開けた。  * prefab-theme/theme.conf (sidebarbgcolor): transparent にして隙間に変な色が出ないように修正。
    git-svn-id: svn://localhost/note-svn/trunk@76 c94b6e96-04ce-b74c-b9b8-e4e85ce117fa

--- conf.py
+++ conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'読書ノート'
-copyright = u'2009, プレハブ小屋'
+copyright = u'2010, プレハブ小屋'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
