4e217563e9ee19f287c12981147d86ea7d3e72a8
showa-yojyo <yojyo@hotmail.com>
2012-03-08 07:25:38 +0000

    * scratch-example.sh: 間違えて実行したときに、既存ファイルを上書きするのはまずい。
    git-svn-id: svn://localhost/note-svn/trunk@243 c94b6e96-04ce-b74c-b9b8-e4e85ce117fa

--- source/scratch-example.sh
+++ source/scratch-example.sh
@@ -3,10 +3,10 @@
 PYTHON='/cygdrive/d/Python26/python.exe'
 
 $PYTHON scratch.py \
-  --title='達人プログラマー' \
+  --title='XXX' \
   --author='Andrew Hunt and David Thomas' \
   --interpreter='村上雅章' \
   --publisher='株式会社ピアソン・エデュケーション' \
   --isbn='ISBN4-89471-274-1' \
   --separate=3 \
-  hunt00
+  xx
