a89d43f1af43ed13a43a92fbcb4c28adca7247c0
showa_yojyo <yojyo@hotmail.com>
2014-11-07 22:21:46 +0900

    Update skeleton.py so that it works as-is.

--- sample/pyopengl/skeleton.py
+++ sample/pyopengl/skeleton.py
@@ -1,14 +1,14 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# OpenGL の演習スクリプト用テンプレ
-"""%prog
-A study program of OpenGL
-Example: %prog"""
+"""A study program of OpenGL."""
+
+# pylint: disable=unused-argument
+# pylint: disable=wildcard-import,unused-wildcard-import
+# pylint: disable=invalid-name
 import sys
 
-# 三角関数等を利用するならばコメントアウトを解除する。
 #import math
-from optparse import OptionParser
-
+from argparse import ArgumentParser
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
@@ -19,19 +19,23 @@ from OpenGL.GLUT import *
 # Pillow
 #from PIL import Image
 
-# main 等で使う変数
-window_title=b"OpenGL Study"
-window_sx,window_sy = 320,240
-window_x,window_y = 100,100
+# Here are constant variables.
+window_title = b"OpenGL Study"
+window_sx, window_sy = 320, 240
+window_x, window_y = 100, 100
 
 def init():
-    """OpenGL ステートを初期化する"""
+    """Initialize the OpenGL state."""
 
     glClearColor(0., 0., 0., 1.)
     glEnable(GL_DEPTH_TEST)
 
 def display():
-    """シーンレンダリング"""
+    """The display callback function.
+
+    Returns:
+      None
+    """
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
@@ -39,7 +43,7 @@ def display():
     glPushMatrix()
     glLoadIdentity()
 
-    # プリミティブを描画する。
+    # Render primitives.
     glPushAttrib(GL_CURRENT_BIT)
 
     glColor3f(1, 0, 0)
@@ -52,14 +56,19 @@ def display():
     glPopAttrib()
     glPopMatrix()
 
-    # バッファスワップ
+    # Swap rendering buffers to repaint the screen.
     glutSwapBuffers()
 
-################################################################################
-# callback
-
 def reshape(width, height):
-    """ウィンドウリサイズイベントをハンドルする。"""
+    """The reshape callback function.
+
+    Args:
+      width (int): the width of new window size in pixels.
+      height (int): the width of new window size in pixels.
+
+    Returns:
+      None
+    """
     if height == 0:
         return
 
@@ -67,21 +76,33 @@ def reshape(width, height):
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
     if width <= height:
-        # 縦長ビューポートのケース
+        # Portrait.
         aspect = float(height) / width
         glOrtho(-2, 2, -2 * aspect, 2 * aspect, -5.0, 20.0)
     else:
-        # 横長ビューポートのケース
+        # Landscape.
         aspect = float(width) / height
         glOrtho(-2 * aspect, 2 * aspect, -2, 2, -5.0, 20.0)
-    # もしくは
+
+    # When you prefer perspective projection, use this:
     #gluPerspective(45.0, float(width)/height, 1.0, 100.0)
 
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
 
 def keyboard(key, x, y):
-    """キーボードイベントをハンドルする。"""
+    """The new keyboard callback function.
+
+    Args:
+      key (?): n The ASCII character that the user typed into the window.
+      x (int): X coordinate of the mouse location in window relative
+        coordinates when the key was pressed.
+      y (int): Y coordinate of the mouse location in window relative
+        coordinates when the key was pressed.
+
+    Returns:
+      None
+    """
 
     if ord(key) == 0o33:
         # ESC
@@ -89,30 +110,57 @@ def keyboard(key, x, y):
         sys.exit()
 
 def mouse(button, state, x, y):
-    """マウスのクリックイベントをハンドルする。"""
+    """The mouse callback function.
+
+    Args:
+        button (int): One of GLUT_LEFT_BUTTON, GLUT_MIDDLE_BUTTON, or
+          GLUT_RIGHT_BUTTON.
+        state (int): either GLUT_UP or GLUT_DOWN.
+        x (int): X of the window relative coordinates when the mouse button
+          state changed.
+        y (int): Y of the window relative coordinates when the mouse button
+          state changed.
+
+    Returns:
+        None.
+    """
     pass
 
 def motion(x, y):
-    """マウスのモーションイベントをハンドルする。"""
+    """The motion callback function.
+
+    Args:
+        x (int): X of the mouse location in window relative coordinates.
+        y (int): Y of the mouse location in window relative coordinates.
+
+    Returns:
+        None.
+    """
     pass
 
-################################################################################
-# main
+def main(args):
+    """The main function.
 
-def main(options, args):
-    # GLUT を初期化する。
+    Args:
+        args: An instance which argparse.ArgumentParser.parse_args() returned.
+
+    Returns:
+        None.
+    """
+
+    # Initialize GLUT.
     glutInit(sys.argv)
 
-    # ウィンドウを設定する。
+    # Initialize and create the main window.
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
     glutInitWindowSize(window_sx, window_sy)
     glutInitWindowPosition(window_x, window_y)
     glutCreateWindow(window_title)
 
-    # OpenGL ステートを初期化する。
+    # Initialize the OpenGL state.
     init()
 
-    # コールバック関数を登録する。
+    # Register callback functions.
     glutDisplayFunc(display)
     glutKeyboardFunc(keyboard)
     glutReshapeFunc(reshape)
@@ -122,11 +170,5 @@ def main(options, args):
     glutMainLoop()
 
 if __name__ == '__main__':
-    parser = OptionParser(__doc__)
-
-    options, args = parser.parse_args()
-    #if len(args) < 1:
-    #    print(parser.print_help(), file=sys.stderr)
-    #    sys.exit(1)
-
-    main(options, args)
+    parser = ArgumentParser(description='OpenGL Study')
+    main(parser.parse_args())
