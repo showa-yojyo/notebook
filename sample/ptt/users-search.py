a424beee66cd15f8f56f1920d9595ec8d94085cd
showa_yojyo <yojyo@hotmail.com>
2014-11-22 01:35:27 +0900

    Add users/search.

---
+++ sample/ptt/users-search.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Demonstration GET users/search
+# See https://dev.twitter.com/docs/api/1.1/get/users/search
+
+from secret import twitter_instance
+
+tw = twitter_instance()
+
+response = tw.users.search(
+    q='bot',
+    page=0,
+    count=20,
+    include_entities=False)
+
+for i in response:
+    print('''
+{screen_name} | {name}
+{location}
+{url}
+{description}
+
+ツイート数 {statuses_count}
+フォロー {friends_count} 人
+フォロワー {followers_count} 人
+'''.format(**i))
+
