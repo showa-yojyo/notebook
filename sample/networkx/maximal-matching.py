4a19c550f6765cac9132de5cddd4e9cb13019106
showa_yojyo <yojyo@hotmail.com>
2014-12-22 02:01:08 +0900

    Update matching.rst and add new related files.

---
+++ sample/networkx/maximal-matching.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""maximal-matching.py: demonstrate maximal_matching.
+"""
+import networkx as nx
+
+def main():
+    """The main function."""
+
+    for edge_list in generate_edges():
+        print(nx.maximal_matching(nx.Graph(edge_list)))
+
+def generate_edges():
+    """Generate edge lists to demonstrate maximal matching.
+
+    Yields:
+      Tuple of integer pairs.
+    """
+
+    # Fig. (a)
+    yield (
+        (0, 1), (0, 2),
+        (1, 2),
+        (3, 1),
+        (4, 1),
+        (5, 1))
+
+    # Fig. (b)
+    yield (
+        (0, 1), (0, 2),
+        (1, 2),
+        (3, 1), (3, 4), (3, 5),
+        (4, 2))
+
+    # Fig. (c)
+    yield (
+        (0, 1), (0, 2),
+        (1, 2),
+        (3, 1), (3, 4),
+        (4, 2))
+
+if __name__ == '__main__':
+    main()
