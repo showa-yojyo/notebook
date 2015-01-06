6b1322d7e987a1a5c8e4c130a5fe148f9d79fd08
showa_yojyo <yojyo@hotmail.com>
2015-01-06 22:33:06 +0900

    Update NetworkX drawing.rst (how to draw a complete graph).

---
+++ sample/networkx/drawing-comp.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""drawing-comp.py: demonstrate NetworkX (circular_layout).
+"""
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+import colorsys
+
+num_nodes = 17
+G = nx.complete_graph(num_nodes)
+
+if False:
+    pos = nx.circular_layout(G)
+else:
+    pos = {}
+    for i in range(num_nodes):
+        theta = 2 * np.pi * i / num_nodes
+        pos[i] = np.array([np.cos(theta), np.sin(theta)], dtype=np.float32)
+
+# Node colors.
+ncolors = [colorsys.hsv_to_rgb(h / num_nodes, 1.0, 1.0)
+           for h in range(num_nodes)]
+
+nx.draw_networkx(G, pos, node_color=ncolors)
+plt.axes().set_aspect('equal', 'datalim')
+plt.show()
