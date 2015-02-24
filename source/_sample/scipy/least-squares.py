7885ae001a7b339a37197decc273bec6aa590211
showa_yojyo <yojyo@hotmail.com>
2015-02-24 21:58:10 +0900

    Revise least-squares.py.

--- source/_sample/scipy/least-squares.py
+++ source/_sample/scipy/least-squares.py
@@ -9,14 +9,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Sampling data set.
-#xd = np.array([18,23,25,35,65,54,34,56,72,19,23,42,18,39,37])
 xd = np.array([72, 67, 65, 55, 25, 36, 56, 34, 18, 71, 67, 48, 72, 51, 53])
-yd = np.array([202,186,187,180,156,169,174,172,153,199,193,174,198,183,178])
+yd = np.array([202, 186, 187, 180, 156, 169, 174, 172, 153, 199, 193, 174, 198, 183, 178])
 
 # Solve the linear least squares problem.
-A = np.vstack((xd, np.ones(xd.shape[0]))).transpose()
+A = np.c_[xd[:, np.newaxis], np.ones(xd.shape[0])]
 B = yd
-X = lstsq(A, B)[0]
+X, residues, rank, s = lstsq(A, B)
 
 # Show the regression curve (line).
 a = X[0]
@@ -25,8 +24,9 @@ print("Line: y = {:.3f}x {:+.3f}".format(a, b))
 
 # Plot both the sampling data and the regression curve.
 plt.figure()
-xs = np.array([min(xd), max(xd)])
+xs = np.r_[min(xd):max(xd):15j]
 ys = a * xs + b
+
 plt.plot(xs, ys, color='deeppink', label='regression curve')
 plt.scatter(xd, yd, color='pink', marker='s', label='data set')
 plt.legend()
