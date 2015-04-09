3f9b6c6e2beba5610639ad667ba689478ea7cfe0
showa_yojyo <yojyo@hotmail.com>
2015-04-09 23:53:30 +0900

    Revise shader.rst, based on glappbase.py.

--- source/_sample/pyopengl/glsl-minimal.py
+++ source/_sample/pyopengl/glsl-minimal.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""glsl-minimal.py
+"""glsl-minimal.py: Demonstrate GLSL (with legacy API).
 
 Reference:
   * rndblnch / opengl-programmable <bitbucket.org/rndblnch/opengl-programmable>
@@ -13,12 +13,8 @@ import sys
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
+from glappbase import GLAppBase
 
-WINDOW_TITLE = b"Simple Demo for GLSL"
-WINDOW_SX, WINDOW_SY = 320, 240
-WINDOW_X, WINDOW_Y = 100, 100
-
-# minimal.vert
 VERT_SHADER_SOURCE = """
 void main()
 {
@@ -26,7 +22,6 @@ void main()
 }
 """
 
-# minimal.frag
 FRAG_SHADER_SOURCE = """
 void main()
 {
@@ -34,56 +29,14 @@ void main()
 }
 """
 
-class GLSLMinimal(object):
+class GLSLMinimal(GLAppBase):
     """A minimal program which demonstrates GLSL."""
 
-    def __init__(self):
+    def __init__(self, **kwargs):
         """Initialize an instance of class GLSLMinimal."""
+        kwargs['context_version'] = (2, 1)
+        super(GLSLMinimal, self).__init__(**kwargs)
         self.rotation = 0
-
-    def run(self, args):
-        """Run a program.
-
-        Args:
-          args: Command line parameters passed to glutInit.
-        """
-        self.init_glut(args)
-
-        print("Vendor: {}\nRenderer: {}\nVersion: {}\nGLSL: {}".format(
-            glGetString(GL_VENDOR).decode(),
-            glGetString(GL_RENDERER).decode(),
-            glGetString(GL_VERSION).decode(),
-            glGetString(GL_SHADING_LANGUAGE_VERSION).decode()),
-              file=sys.stderr, flush=True)
-
-        self.init_program()
-        self.init_opengl()
-        return glutMainLoop()
-
-    def init_glut(self, args):
-        """Initialize the GLUT state.
-
-        Args:
-          args: Command line parameters passed to glutInit.
-        """
-
-        glutInit(args)
-
-        glutInitContextVersion(2, 1)
-        glutInitContextFlags(GLUT_FORWARD_COMPATIBLE)
-        glutInitContextProfile(GLUT_CORE_PROFILE)
-
-        glutInitDisplayMode(
-            GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH)
-
-        glutInitWindowSize(WINDOW_SX, WINDOW_SY)
-        glutInitWindowPosition(WINDOW_X, WINDOW_Y)
-        glutCreateWindow(WINDOW_TITLE)
-
-        glutDisplayFunc(self.display)
-        glutIdleFunc(self.display)
-        glutReshapeFunc(self.reshape)
-        glutKeyboardFunc(self.keyboard)
 
     def init_program(self):
         """Setup shaders."""
@@ -113,17 +66,19 @@ class GLSLMinimal(object):
 
         glUseProgram(program)
 
-    def init_opengl(self):
+    def init_gl(self):
         """Initialize the OpenGL state."""
 
         glEnable(GL_DEPTH_TEST)
-        glClearColor(1.0, 1.0, 1.0, 1.0)
         glEnable(GL_CULL_FACE)
+        glClearColor(1.0, 1.0, 1.0, 1.0)
 
-    def display(self):
-        """The display callback function."""
+    def do_render(self):
+        """Render the object."""
 
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glMatrixMode(GL_MODELVIEW)
+        glPushAttrib(GL_ALL_ATTRIB_BITS)
+        glPushMatrix()
         glLoadIdentity()
         gluLookAt(
             0.0, 0.0, 5.0,
@@ -134,55 +89,27 @@ class GLSLMinimal(object):
         glRotatef(self.rotation, 0, 1, 1)
         glutSolidTeapot(1)
 
+        glPopAttrib()
+        glPopMatrix()
         self.rotation += 0.1
 
-        glutSwapBuffers()
+    def resize(self, width, height):
+        """The reshape callback function."""
 
-    def reshape(self, width, height):
-        """The reshape callback function.
-
-        Args:
-          width (int): the width of new window size in pixels.
-          height (int): the width of new window size in pixels.
-        """
-
-        # Prevent a divide by zero, when window is too short.
         if height == 0:
             return
 
-        # Reset the coordinate system before modifying.
         glMatrixMode(GL_PROJECTION)
         glLoadIdentity()
 
-        # Set the viewport to be the entire window.
         glViewport(0, 0, width, height)
 
-        # Set the correct perspective.
         gluPerspective(45, width / height, 1, 1000)
-        glMatrixMode(GL_MODELVIEW)
-
-    def keyboard(self, key, x, y):
-        """The keyboard callback function.
-
-        Args:
-          key (?): n The ASCII character that the user typed into the window.
-          x (int): X coordinate of the mouse location in window relative
-            coordinates when the key was pressed.
-          y (int): Y coordinate of the mouse location in window relative
-            coordinates when the key was pressed.
-        """
-
-        if ord(key) == 0o33 or key == b'q':
-            print('Exit program.', file=sys.stderr, flush=True)
-            sys.exit(0)
 
 def main(args):
-    """The main function.
+    """The main function."""
 
-    Args:
-        args: Command line parameters passed to glutInit.
-    """
-    app = GLSLMinimal()
+    app = GLSLMinimal(window_title=b"Simple Demo for GLSL")
     app.run(sys.argv)
 
 if __name__ == "__main__":
