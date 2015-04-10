ef1dd0c52341b4d175c568a1001fe515103ebc3b
showa_yojyo <yojyo@hotmail.com>
2015-04-10 22:42:53 +0900

    Reform legacy-god.py.

--- source/_sample/pyopengl/legacy-god.py
+++ source/_sample/pyopengl/legacy-god.py
@@ -1,70 +1,18 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+"""
+# pylint: disable=unused-argument,no-self-use
+# pylint: disable=wildcard-import,unused-wildcard-import
+# pylint: disable=invalid-name
 import sys
-
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
-
-# NumPy
-from numpy import array
-
-# PIL
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
-
-# main 等で使う変数
-window_title=b"God"
-window_sx,window_sy = 320,240
-window_x,window_y = 100,100
-
-def init():
-    """OpenGL ステートを初期化する"""
-
-    glClearColor(0., 0., 0., 1.)
-    glEnable(GL_DEPTH_TEST)
-    #glEnable(GL_LIGHTING)
-    #glEnable(GL_LIGHT0)
-    #glLightfv(GL_LIGHT0, GL_POSITION, array( (5, 5, 10, 0.0),'f') );
-
-    #glMaterialfv(GL_FRONT, GL_SPECULAR, array((1.0, 1.0, 1.0, 0.15),'f') );
-    #glMaterialfv(GL_FRONT, GL_SHININESS, array((100.0, ),'f') );
-    
-    #glEnable(GL_BLEND)
-
-    textwidth, textheight = 64, 64
-    img = drawtext(textwidth, textheight)
-
-    glEnable(GL_TEXTURE_2D)
-    glTexImage2D(GL_TEXTURE_2D, 0, 4, textwidth, textheight, 0, GL_RGBA, GL_UNSIGNED_BYTE, img.tostring())
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP)
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP)
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
-
-def drawtext(textwidth, textheight):
-    # 大きめのキャンヴァスを用意しておく。
-    img = Image.new('RGBA', (textwidth, textheight), 'white')
-    #return img
-
-    dr = ImageDraw.Draw(img)
-    # HG 明朝体を使ってみる。
-    fnt = ImageFont.truetype('hgrme.ttc', 48, encoding='utf-8')
-
-    # テキストを用意する。
-    text = u'神'
-    
-    width = 6
-    height = 10
-    for line in text.splitlines():
-        ext = dr.textsize(line, fnt)
-        dr.text((width, height), line, font=fnt, fill='black')
-        width = max(ext[0], width)
-        height += ext[1]
-    
-    # 余白をトリムする。
-    #img = img.crop((0, 0, width, height))
-    return img
+from glappbase import GLAppBase
 
 vertices = [[-1.0, -1.0,  1.0],
             [-1.0,  1.0,  1.0],
@@ -75,129 +23,105 @@ vertices = [[-1.0, -1.0,  1.0],
             [1.0,  1.0,  -1.0],
             [1.0, -1.0,  -1.0]]
 
-colors = [[1.0, 0.0, 0.0],
-          [0.0, 1.0, 1.0],
-          [1.0, 1.0, 0.0],
-          [0.0, 1.0, 0.0],
-          [0.0, 0.0, 1.0],
-          [1.0, 0.0, 1.0],
-          [0.0, 0.0, 0.0],
-          [1.0, 1.0, 1.0]]
+def drawtext(text, textwidth, textheight):
+
+    img = Image.new('RGBA', (textwidth, textheight), 'white')
+    dr = ImageDraw.Draw(img)
+    fnt = ImageFont.truetype('hgrme.ttc', 48, encoding='utf-8')
+
+    width = 6
+    height = 10
+    for line in text.splitlines():
+        ext = dr.textsize(line, fnt)
+        dr.text((width, height), line, font=fnt, fill='black')
+        width = max(ext[0], width)
+        height += ext[1]
+
+    return img
 
 def polygon(a, b, c, d):
     glBegin(GL_POLYGON)
-    #glColor3fv(colors[a])
+
     glTexCoord2f(0.0, 0.0)
     glVertex3fv(vertices[a])
 
-    #glColor3fv(colors[b])
     glTexCoord2f(0.0, 1.0)
     glVertex3fv(vertices[b])
 
-    #glColor3fv(colors[c])
     glTexCoord2f(1.0, 1.0)
     glVertex3fv(vertices[c])
 
-    #glColor3fv(colors[d])
     glTexCoord2f(1.0, 0.0)
     glVertex3fv(vertices[d])
     glEnd()
 
-def display():
-    """シーンレンダリング"""
+class TextOnCubeDemo(GLAppBase):
+    """Another demonstration of rendering text."""
 
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    def __init__(self, **kwargs):
+        """Initialize an instance of this class."""
 
-    glMatrixMode(GL_MODELVIEW)
-    glPushMatrix()
-    glLoadIdentity()
+        kwargs['context_version'] = (1, 5)
+        super(TextOnCubeDemo, self).__init__(**kwargs)
 
-    gluLookAt(4, 4, 3,
-              0, 0, 0,
-              0, 0, 1)
+    def init_gl(self):
+        """Initialize the OpenGL state."""
 
-    # プリミティブを描画する。
-    glPushAttrib(GL_CURRENT_BIT)
+        glClearColor(0.0, 0.0, 0.0, 1.0)
+        glEnable(GL_DEPTH_TEST)
+        glEnable(GL_TEXTURE_2D)
 
-    polygon(0, 3, 2, 1)
-    polygon(2, 3, 7, 6)
-    polygon(3, 0, 4, 7)
-    polygon(1, 2, 6, 5)
-    polygon(4, 5, 6, 7)
-    polygon(5, 4, 0, 1)
+    def init_object(self):
+        """Initialize the object to be displayed."""
 
-    glPopAttrib()
-    glPopMatrix()
+        textwidth, textheight = 64, 64
+        img = drawtext('神', textwidth, textheight)
 
-    # バッファスワップ
-    glutSwapBuffers()
+        glTexImage2D(GL_TEXTURE_2D, 0, 4, textwidth, textheight, 0, GL_RGBA, GL_UNSIGNED_BYTE, img.tostring())
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
 
-################################################################################
-# callback
+    def resize(self, width, height):
+        """The reshape callback function."""
 
-def reshape(width, height):
-    """ウィンドウリサイズイベントをハンドルする。"""
-    if height == 0:
-        return
+        if height == 0:
+            return
 
-    glViewport(0, 0, width, height)
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    #if width <= height:
-    #    # 縦長ビューポートのケース
-    #    aspect = float(height) / width
-    #    glOrtho(-2, 2, -2 * aspect, 2 * aspect, -5.0, 20.0)
-    #else:
-    #    # 横長ビューポートのケース
-    #    aspect = float(width) / height
-    #    glOrtho(-2 * aspect, 2 * aspect, -2, 2, -5.0, 20.0)
-    ## もしくは
-    gluPerspective(45.0, float(width)/height, 1.0, 100.0)
+        glViewport(0, 0, width, height)
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        gluPerspective(45.0, width / height, 1.0, 100.0)
 
-    glMatrixMode(GL_MODELVIEW)
-    glLoadIdentity()
+    def do_render(self):
+        """The display callback function."""
 
-def keyboard(key, x, y):
-    """キーボードイベントをハンドルする。"""
+        glMatrixMode(GL_MODELVIEW)
+        glPushMatrix()
+        glLoadIdentity()
 
-    if ord(key) == 0o33:
-        # ESC
-        print('ESC pressed', file=sys.stderr)
-        sys.exit()
+        gluLookAt(4, 4, 3,
+                  0, 0, 0,
+                  0, 0, 1)
 
-def mouse(button, state, x, y):
-    """マウスのクリックイベントをハンドルする。"""
-    pass
+        glPushAttrib(GL_CURRENT_BIT)
 
-def motion(x, y):
-    """マウスのモーションイベントをハンドルする。"""
-    pass
+        polygon(0, 3, 2, 1)
+        polygon(2, 3, 7, 6)
+        polygon(3, 0, 4, 7)
+        polygon(1, 2, 6, 5)
+        polygon(4, 5, 6, 7)
+        polygon(5, 4, 0, 1)
 
-################################################################################
-# main
+        glPopAttrib()
+        glPopMatrix()
 
-def main():
-    # GLUT を初期化する。
-    glutInit(sys.argv)
+def main(args):
+    """The main function."""
 
-    # ウィンドウを設定する。
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
-    glutInitWindowSize(window_sx, window_sy)
-    glutInitWindowPosition(window_x, window_y)
-    glutCreateWindow(window_title)
+    app = TextOnCubeDemo(window_title=b"God")
+    app.run(sys.argv)
 
-    # OpenGL ステートを初期化する。
-    init()
-
-    # コールバック関数を登録する。
-    glutDisplayFunc(display)
-    glutKeyboardFunc(keyboard)
-    glutReshapeFunc(reshape)
-    glutMouseFunc(mouse)
-    #glutMotionFunc(motion)
-
-    # イベントハンドルスタート。
-    glutMainLoop()
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
