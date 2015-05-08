641741a2f92711fe0d313387f2f2080505aba0a4
showa_yojyo <yojyo@hotmail.com>
2015-05-08 22:03:21 +0900

    Fix hello world.

--- source/_sample/pillow/text1.py
+++ source/_sample/pillow/text1.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 from PIL import Image, ImageDraw
 
-IMAGE_WIDTH, IMAGE_HEIGHT = 320, 240
-TEXT_COLOR = 'red'
+IMAGE_WIDTH, IMAGE_HEIGHT = 96, 24
+TEXT_COLOR = 'white'
 
 # デフォルト背景色のキャンヴァスを用意する。
-img = Image.new('RGBA', (IMAGE_WIDTH, IMAGE_HEIGHT))
+img = Image.new('RGBA', (IMAGE_WIDTH, IMAGE_HEIGHT), 'black')
 
 # Draw 関数でオブジェクトを作成。
 draw = ImageDraw.Draw(img)
 
-# 画面の左上隅にテキストを赤く描画する。
+# 画面の左上隅にテキストを描画する。
 draw.text((0, 0), 'Hello, world', fill=TEXT_COLOR)
