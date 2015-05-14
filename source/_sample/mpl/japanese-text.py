689746803d3e0e4004326eea16abe81dba0119b3
showa_yojyo <yojyo@hotmail.com>
2015-05-15 00:11:29 +0900

    Update japanese-text.py

--- source/_sample/mpl/japanese-text.py
+++ source/_sample/mpl/japanese-text.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""japanese-text.py: Demonstrate how to draw Japanese text.
+"""
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib.font_manager import FontProperties
 
-fp = FontProperties(fname=r'C:\WINDOWS\Fonts\HGRME.ttc') # HG 明朝系
+fp = FontProperties(fname=r'C:\WINDOWS\Fonts\HGRME.TTC') # HG 明朝系
 plt.text(0, 0, '御無礼\n一発です', fontproperties=fp, fontsize=60)
 plt.show()
