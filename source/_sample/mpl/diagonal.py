9e35ed3bb881bae29c5492c559af6290f36a9868
showa_yojyo <yojyo@hotmail.com>
2015-05-14 21:52:50 +0900

    Improve Matplotlib demonstration scripts.

--- source/_sample/mpl/diagonal.py
+++ source/_sample/mpl/diagonal.py
@@ -1,4 +1,8 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""diagonal.py: Demonstrate how to draw a diagonal line which joins
+two opposite corners of the figure.
+"""
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 fig = plt.figure()
--- source/_sample/mpl/polynomial.py
+++ source/_sample/mpl/polynomial.py
@@ -1,4 +1,7 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""polynomial.py: Demonstrate how to draw a polynomial curve.
+"""
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
@@ -6,16 +9,16 @@ import numpy as np
 fig = plt.figure()
 ax1 = fig.add_subplot(111)
 
-f = np.poly1d([1, -4, 3, 0]) # x = 0, 1, 3 を根に持つ 3 次多項式。
-xs = np.arange(-2, 4, 0.1) # x in [-2, 4] を 0.1 刻みでプロット。
-ys = [f(x) for x in xs] # xs と対になる ys
-ax1.plot(xs, ys)
+# Let f be the polynomial whose roots are 0, 1, and 3.
+f = np.poly1d([1, -4, 3, 0])
+x = np.linspace(-2, 4, 50)
 
-# x = [-1, 3] から 1 間隔に接線を引く。
+# Draw the curve of y = f(x).
+ax1.plot(x, f(x))
+
+# Draw tangent lines at x = -1, 0, .., 3.
 slope = f.deriv()
-for a in range(-1, 3, 1):
-    b = f(a)
-    ys1 = [slope(a) * (x - a) + b for x in xs]
-    ax1.plot(xs, ys1)
+for i in range(-1, 4):
+    ax1.plot(x, slope(i) * (x - i) + f(i))
 
 plt.show()
