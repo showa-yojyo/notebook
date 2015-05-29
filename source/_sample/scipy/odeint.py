c770aca29bab835a200decabed468b43eebc84bc
showa_yojyo <yojyo@hotmail.com>
2015-05-29 22:37:16 +0900

    Fix comment.

--- source/_sample/scipy/odeint.py
+++ source/_sample/scipy/odeint.py
@@ -9,7 +9,7 @@ from scipy.integrate import odeint
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Solve y''(t) + a y(t) + b y'(t) == 0.
+# Solve y''(t) + a y'(t) + b y(t) == 0.
 
 def deriv(y, t):
     """Return derivatives of the array y."""
