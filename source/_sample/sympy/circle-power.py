64b7bb27a77125c4595e56c2f7792d8e4d3c972c
showa_yojyo <yojyo@hotmail.com>
2015-06-12 22:49:44 +0900

    Update geometry.rst (power-of-a-point theorem demonstration).

---
+++ source/_sample/sympy/circle-power.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Demonstrate power-of-a-point theorem with SymPy."""
+from sympy import Point, Line, Circle
+
+def demonstrate_circle_power(circle):
+    """Demonstrate power-of-a-point theorem."""
+
+    A, B, C, D = (circle.random_point() for i in range(4))
+    P = Line.intersection(Line(A, B), Line(C, D))
+    assert len(P) == 1
+    P = P[0]
+
+    PAPB = P.distance(A) * P.distance(B)
+    PCPD = P.distance(C) * P.distance(D)
+
+    print("PA * PB =", PAPB.evalf())
+    print("PC * PD =", PCPD.evalf())
+
+center = Point(0, 0)
+radius = 1
+demonstrate_circle_power(Circle(center, radius))
