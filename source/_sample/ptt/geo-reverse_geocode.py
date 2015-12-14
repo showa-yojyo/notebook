c74904cade36100a517300da657615e22cfe3f25
showa_yojyo <yojyo@hotmail.com>
2015-12-14 22:29:04 +0900

    (rest-geo.rst) Add this file and related script files.

---
+++ source/_sample/ptt/geo-reverse_geocode.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Demonstration GET geo/reverse_geocode
+# See https://dev.twitter.com/rest/reference/get/geo/reverse_geocode
+
+from secret import twitter_instance
+from json import dump
+import sys
+
+tw = twitter_instance()
+
+# [1]
+response = tw.geo.reverse_geocode(lat=35.696805, long=139.773828)
+
+# [2]
+dump(response, sys.stdout, ensure_ascii=False, indent=4)
