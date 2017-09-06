32b96b642e43f318920a64235e52c5d7de6b8031
showa_yojyo <yojyo@hotmail.com>
2017-09-06 23:44:35 +0900

    (tools) Update scratch.py and the template set

--- tools/scratch.py
+++ tools/scratch.py
@@ -3,11 +3,11 @@
 
 Examples:
   $ scratch.py milestone09
-  $ scratch.py gamma95 --separate=3
+  $ scratch.py gamma95 --pages=3
 """
 from argparse import ArgumentParser
-from jinja2 import Environment
-from jinja2 import FileSystemLoader
+from pathlib import Path
+from jinja2 import (Environment, FileSystemLoader)
 
 def main(args):
     """The main function.
@@ -15,38 +15,42 @@ def main(args):
     Args:
       args: An instance of argparse.ArgumentParser after
         parse_args() called.
-
-    Returns:
-        None.
     """
 
-    env = Environment(loader=FileSystemLoader('.'))
-    filename = args.file_name[0]
-    num_page = args.separate
+    env = Environment(loader=FileSystemLoader(
+        str(Path(__file__).parent)))
+    note_name = args.note_name[0]
+    num_page = args.pages
 
     params = dict(title=args.title,
-                  author=args.author,
-                  interpreter=args.interpreter,
-                  publisher=args.publisher,
-                  isbn=args.isbn,
-                  notename=filename)
+                  notename=note_name)
 
+    # Case of a single page is specified.
     if num_page == 1:
         template = env.get_template(args.page_template)
         params['page'] = 0
         params['totalpages'] = 0
-    else:
-        template = env.get_template(args.cover_template)
 
-    with open('{}.rst'.format(filename), mode='w', encoding='utf-8') as fout:
-        print(template.render(params), file=fout)
+        with open('{}.rst'.format(note_name), mode='w', encoding='utf-8') as fout:
+            print(template.render(params), file=fout)
 
-    if num_page == 1:
         return
 
+    # Case of an exclusive directory, index.rst, and note[1-9].rst.
+
+    # Execute `mkdir -p note_name`.
+    dest = Path.cwd() / note_name
+    dest.mkdir(exist_ok=True)
+
+    # Generate index.rst.
+    template = env.get_template(args.index_template)
+    with open(dest / 'index.rst', mode='w', encoding='utf-8') as fout:
+        print(template.render(params), file=fout)
+
+    # Generate note[1-9].rst.
     template = env.get_template(args.page_template)
     for i in range(1, num_page + 1):
-        with open('{}-note{:d}.rst'.format(filename, i),
+        with open(dest / 'note{:d}.rst'.format(i),
                   mode='w', encoding='utf8') as fout:
             print(template.render(params, page=i, totalpages=num_page),
                   file=fout)
@@ -54,41 +58,25 @@ def main(args):
 if __name__ == '__main__':
     parser = ArgumentParser(description='Generate note templates.')
     parser.add_argument(
-        'file_name',
+        'note_name',
         nargs=1,
-        help='TBW')
+        help='specify the output file or directory to be generated')
     parser.add_argument(
         '--title',
-        default='タイトル',
-        help='TBW')
+        default='TODO: Insert title here',
+        help='specify the title of your note')
     parser.add_argument(
-        '--author',
-        default='著者',
-        help='TBW')
-    parser.add_argument(
-        '--interpreter',
-        default='訳者',
-        help='TBW')
-    parser.add_argument(
-        '--publisher',
-        default='出版社',
-        help='TBW')
-    parser.add_argument(
-        '--isbn',
-        default='ISBN-XXXXX',
-        help='TBW')
-    parser.add_argument(
-        '--separate',
+        '--pages',
         type=int,
         default='1',
-        help='TBW')
+        help='specify the number of pages (default to 1)')
     parser.add_argument(
-        '--cover_template',
+        '--index_template',
         default='toc.rst_t',
-        help='TBW')
+        help='specify the template file name for TOC page (default to toc.rst_t)')
     parser.add_argument(
         '--page_template',
         default='note.rst_t',
-        help='TBW')
+        help='specify the template file name for all pages (default to note.rst_t)')
 
     main(parser.parse_args())
