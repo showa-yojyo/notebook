9fb02735b426ad4aa65a3bb14824563339677af2
showa_yojyo <yojyo@hotmail.com>
2022-05-29 10:33:29 +0900

    tools/cleanup-rst: a pandoc post-processor

---
+++ tools/cleanup-rst.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SOURCE=$1
+COLUMNS=70
+
+section1=$(python -c "print(\"=\" * $COLUMNS, end=\"\")")
+section2=$(python -c "print(\"-\" * $COLUMNS, end=\"\")")
+section3=$(python -c "print(\"^\" * $COLUMNS, end=\"\")")
+section4=$(python -c "print(\"~\" * $COLUMNS, end=\"\")")
+
+sed -e "s/^=\+$/$section1/" -e "s/^-\+$/$section2/" -e "s/^\+$/$section3/" -e "s/^~\+$/$section4/g" $SOURCE;
