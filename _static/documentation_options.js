00d2ee95f8936da21747d7452e594921bdb25844
showa-yojyo <showa-yojyo@users.noreply.github.com>
2023-10-26 04:05:23 +0000

    deploy: 737c8599534573fb24a40367722d89c604857ced

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -6,9 +6,9 @@ var DOCUMENTATION_OPTIONS = {
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
     LINK_SUFFIX: '.html',
-    HAS_SOURCE: true,
+    HAS_SOURCE: false,
     SOURCELINK_SUFFIX: '.txt',
     NAVIGATION_WITH_KEYS: false,
-    SHOW_SEARCH_SUMMARY: true,
+    SHOW_SEARCH_SUMMARY: false,
     ENABLE_SEARCH_SHORTCUTS: true,
 };
