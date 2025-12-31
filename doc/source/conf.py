feae0e93e747d8e0ba43ec1a4fe042591168563d
showa_yojyo <2386769+showa-yojyo@users.noreply.github.com>
2025-12-31 13:50:53 +0900

    Release version 1.7

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,11 +22,11 @@ copyright = f'1999-{date.today().year}, {author} All rights reserved'
 
 # The short X.Y version.
 # The major project version, used as the replacement for |version|.
-version = '1.6'
+version = '1.7'
 
 # The full project version, used as the replacement for |release| and e.g. in
 # the HTML templates.
-release = '1.6dev'
+release = '1.7'
 
 # -- General configuration -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
